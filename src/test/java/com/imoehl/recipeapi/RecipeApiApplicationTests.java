26d78cf321340a93c521f2e002e2c50c81104411
DJ Imoehl <dj.imoehl@gmail.com>
2024-04-05 08:44:43 -0600

    Initial commit

---
+++ src/test/java/com/imoehl/recipeapi/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imoehl.recipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
