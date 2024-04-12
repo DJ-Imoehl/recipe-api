77489babbdf33650b1e1c4018e20b7938409db71
DJ Imoehl <dj.imoehl@gmail.com>
2024-04-12 16:04:03 -0600

    Adding Category

---
+++ src/main/java/com/imoehl/recipeapi/models/Category.java
@@ -0,0 +1,44 @@
+package com.imoehl.recipeapi.models;
+
+import java.util.Objects;
+
+;
+public class Category {
+    private CategoryType name;
+    private String photo;
+
+    public Category(CategoryType name, String photo) {
+        this.name = name;
+        this.photo = photo;
+    }
+
+
+    public CategoryType getName() {
+        return name;
+    }
+
+    public void setName(CategoryType name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return name == category.name && Objects.equals(photo, category.photo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, photo);
+    }
+}
---
+++ src/main/java/com/imoehl/recipeapi/models/CategoryType.java
@@ -0,0 +1,15 @@
+package com.imoehl.recipeapi.models;
+
+public enum CategoryType{
+    BREAKFAST,
+    LUNCH,
+    DINNER,
+    SALAD,
+    VEGETARIAN,
+    QUICK,
+    DESSERT,
+    PASTA,
+    HEALTHY,
+    MEAL_PREP,
+    SOUP
+}
