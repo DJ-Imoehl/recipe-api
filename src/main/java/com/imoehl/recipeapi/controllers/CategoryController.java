0cb48340d4959f89246b312258e4e3c42248be80
DJ Imoehl <dj.imoehl@gmail.com>
2024-04-18 15:04:05 -0600

    Adding featured categories endpoint

--- src/main/java/com/imoehl/recipeapi/controllers/CategoryController.java
+++ src/main/java/com/imoehl/recipeapi/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package com.imoehl.recipeapi.controllers;
 
+import com.fasterxml.jackson.databind.ser.std.IterableSerializer;
 import com.imoehl.recipeapi.assemblers.CategoryAssembler;
 import com.imoehl.recipeapi.assemblers.RecipeAssembler;
 import com.imoehl.recipeapi.exceptions.CategoryNotFoundException;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -54,5 +57,16 @@ public class CategoryController {
                 .getRecipes().stream().map(recipeAssembler::toModel).collect(Collectors.toList());
 
         return CollectionModel.of(recipes, linkTo(methodOn(CategoryController.class).getCategoryRecipes(id)).withRel("/categories/" + id + "/recipes"));
+    }
+
+    @GetMapping("/categories/featured")
+    public CollectionModel<EntityModel<Category>> getFeaturedCategories() {
+        List<Category> featuredCategories =
+                new ArrayList<>(categoryRepository.findAllById(Arrays.asList(1L, 2L, 3L, 4L)));
+
+        List<EntityModel<Category>> categories = featuredCategories.stream()
+                .map(categoryAssembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(categories, linkTo(methodOn(CategoryController.class).all()).withSelfRel());
     }
 }
